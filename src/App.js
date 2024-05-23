bd1dd9eef2accc32bc50fd90c1cdec00d806ec66
Debiddo <dzheng@terpmail.umd.edu>
2024-05-23 02:12:33 -0400

    Single Page React App

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
-import './App.css';
+import BlogDetails from './BlogDetails';
+import Create from './Create';
+import Navbar from './Navbar';
+import NotFound from './NotFound';
+import Home from './home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   const title = "Welcome to the New Blog"
   const likes = 50;
   const person = {name:"yoshi", age:30}
   return (
-    <div className="App">
-      <div className="content">
-        <h1>{title}</h1>
-        <h1>Liked {likes} times</h1>
-        <p>{10}</p>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home/>
+            </Route>
+            <Route path="/create">
+              <Create/>
+            </Route>
+            <Route path="/blogs/:id">
+              <BlogDetails/>
+            </Route>
+            <Route path="*">
+              <NotFound/>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
